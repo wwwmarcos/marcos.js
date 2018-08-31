ba358cc55e4ad075f706f72ba9bd1eed5a2512cb
eptaccio <eptaccio@gmail.com>
2018-08-31 13:34:10 -0300

    feat: update info

---
+++ about.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'Marcos Florencio',
+  github: 'https://github.com/eptaccio',
+  twitter: 'https://twitter.com/eptaccio',
+  linkedin: 'https://www.linkedin.com/in/eptaccio',
+  facebook: 'https://fb.com/eptaccio',
+  telegram: 'https://t.me/eptaccio',
+  email: 'eptaccio@gmail.com',
+  medium: 'https://medium.com/@eptaccio'
+}
--- bin.js
+++ bin.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-console.log(JSON.stringify({
-  name: 'Marcos Florencio',
-  github: 'https://github.com/eptaccio',
-  twitter: 'https://twitter.com/eptaccio',
-  linkedin: 'https://www.linkedin.com/in/marcoseff',
-  facebook: false,
-  telegram: 'https://telegram.me/eptaccio',
-  email: 'eptaccio@gmail.com',
-  medium: 'https://medium.com/@eptaccio'
-}, null, 2))
+const about = require('./about')
+
+console.log(JSON.stringify(about, null, 2))
