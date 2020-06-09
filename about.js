9d9ecb4a65cdb9071fa90120211249266f41407d
Marcos Florencio <eptaccio@gmail.com>
2020-06-09 18:33:51 -0300

    Update about.js

--- about.js
+++ about.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'Marcos Florencio',
+  blog: 'https://eptaccio.dev',
   github: 'https://github.com/eptaccio',
   twitter: 'https://twitter.com/eptaccio',
   linkedin: 'https://www.linkedin.com/in/eptaccio',
-  facebook: 'https://fb.com/eptaccio',
   telegram: 'https://t.me/eptaccio',
   email: 'eptaccio@gmail.com',
   medium: 'https://medium.com/@eptaccio'
